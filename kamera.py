c05ffecd0747f38a49c0b69682985c290af970eb
ardabariskarakoc <ardabariskarakoc@gmail.com>
2023-06-24 15:47:46 +0300

    change canseled character

--- kamera.py
+++ kamera.py
@@ -6,8 +6,8 @@ while True:
     ret, frame = cap.read()
     if frame is not None:
         cv2.imshow('Camera',frame)
-    q = cv2.waitKey(1)
-    if q== ord('q'):
+    a = cv2.waitKey(1)
+    if a== ord('a'):
         break
 
 cap.release()
